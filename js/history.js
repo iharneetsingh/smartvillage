088fad2037058c5e5cbd1fa1f4c5422e56ca9d40
chetangulati <chetan.gulati75@gmail.com>
2018-01-16 12:19:35 +0530

    tab functionality

--- js/history.js
+++ js/history.js
@@ -1,9 +1,18 @@
-$('.menuBtn').click(function () {
+function sideNav() {
   $("#sideNav").toggleClass('open');
   $("#filter").toggleClass('open');
+}
+
+$('.menuBtn').click(function () {
+  sideNav();
 });
 
 $('#filter').click(function (e) {
-  $("#sideNav").toggleClass('open');
-  $("#filter").toggleClass('open');
+  sideNav();
+});
+$('.list').click(function () {
+  $('.tab').css("display","none");
+  var tab = this.getAttribute('data-tab');
+  $(tab).css("display","block");
+  sideNav();
 });
