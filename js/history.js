8b15db710a51ecc35ab3399973a62346f223f13c
chetangulati <chetan.gulati75@gmail.com>
2018-02-02 20:04:53 +0530

    home updates

--- js/history.js
+++ js/history.js
@@ -11,6 +11,8 @@ $('#filter').click(function (e) {
   sideNav();
 });
 $('.list').click(function () {
+  $('.list').removeClass('active');
+  this.className = 'list active';
   $('.tab').css("display","none");
   var tab = this.getAttribute('data-tab');
   $(tab).css("display","block");
